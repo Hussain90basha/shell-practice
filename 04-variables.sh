9bc9f49fc045985abfec4ef24c84a3cd7d13aa09
Hussain90basha <bashahussain9010@gmail.com>
2025-10-14 14:25:08 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=
+PERSON2=
+
+echo "Trump::Welcome to Alaska Putin"
+echo "Putin::Thank you Trump, Finally not on zoom webiner"
+echo "Trump::so, do we have a deal"
+echo "Putin::yes, if you make Ukraine joins Russia"

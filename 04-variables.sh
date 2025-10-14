51d2f5c4a2bd6af4b2d00fbceabe1956b2e4e81d
Hussain90basha <bashahussain9010@gmail.com>
2025-10-14 14:38:53 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1::Welcome to Alaska $PERSON2"
+echo "$PERSON1::Welcome to Alaska $$PERSON2"
 echo "$PERSON2::Thank you $PERSON1, Finally not on zoom webiner"
 echo "$PERSON1::so, do we have a deal"
 echo "$PERSON2::yes, if you make Ukraine joins Russia"

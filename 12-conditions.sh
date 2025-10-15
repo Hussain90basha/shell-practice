0e6cd5fa3604e461bceee2bd626031121b6fbe26
Hussain90basha <bashahussain9010@gmail.com>
2025-10-15 17:38:36 +0530

    conditions

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -3,8 +3,23 @@
 echo "Please enter the number:"
 read NUMBER
 
-if  $(( $NUMBER % 2 -lt 0)); then
-    echo "Given number $NUMBER is PRIME NUMBER"
+if (( NUMBER <= 1 )); then
+    echo "$NUMBER is not a prime number."
+    exit
+fi
+
+is_prime=1
+
+for (( i=2; i*i<=NUMBER; i++ ))
+do
+    if (( NUMBER % i == 0 )); then
+        is_prime=0
+        break
+    fi
+done
+
+if (( is_prime == 1 )); then
+    echo "$NUMBER is a prime number."
 else
-    echo "Given number $NUMBER is PRIME NUMBER OR NOT"   
-fi    
+    echo "$NUMBER is not a prime number."
+fi

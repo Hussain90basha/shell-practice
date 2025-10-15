0949f3cf92200902ab858fdb645b30c8f6cc6f30
Hussain90basha <bashahussain9010@gmail.com>
2025-10-15 17:21:51 +0530

    conditions

---
+++ 12-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter the prime number:"
+read NUMBER
+
+if [ $(( $NUMBER % 2 -lt 30))]; then
+    echo "Given number $NUMBER is PRIME NO."
+else
+    echo "Given number $NUMBER is PRIME NO. OR NOT"   
+fi    

751d42034e45184adeae4bf0c92b87cd1907c61d
Hussain90basha <bashahussain9010@gmail.com>
2025-10-15 12:51:29 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,5 +2,5 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
-echo "Current directory: $pwd"
 echo "Script name: $0"
+echo "Current directory: $pwd"

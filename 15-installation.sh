9ee7145be64088a11e7252a193d6942ce0c8fcb0
Hussain90basha <bashahussain9010@gmail.com>
2025-10-16 13:07:47 +0530

    installation

--- 15-installation.sh
+++ 15-installation.sh
@@ -8,19 +8,19 @@ if [ $USERID -ne 0 ]; then
 fi
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-    echo "ERROR:: Installing $2 is failure"
-    exit 1
-else
-    echo "Installing $2 is success"
+       echo "ERROR:: Installing $2 is failure"
+       exit 1
+    else
+       echo "Installing $2 is success"
  fi
 }
 
 dnf install mysql -y
 VALIDATE $? "MYSQL"
 
- dnf insatll nginx -y
+ dnf install nginx -y
 VALIDATE $? "Nginx"
 
- dnf insatll python3 -y
+ dnf install python3 -y
  VALIDATE $? "python3"
  

8273905c7efa1d160b8fc3c8e363963463e926b6
Hussain90basha <bashahussain9010@gmail.com>
2025-10-16 12:40:50 +0530

    installation

---
+++ 14-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # Failure is another than 0
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MYSQL is failure"
+    exit 1
+else
+    echo "Installing MYSQL is success"
+ fi           
+
+
+

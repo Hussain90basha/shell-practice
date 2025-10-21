d22d1db07bb264f1330722d0ddda26f79eddd7f3
Hussain90basha <bashahussain9010@gmail.com>
2025-10-21 18:45:03 +0530

    practice

--- 21-set.sh
+++ 21-set.sh
@@ -2,7 +2,13 @@
 
 set -e 
 
+error(){
+    echo "There is an error $LINENO, command is: $BASH_COMMAND"
+
+}
+trap error ERR
+
 echo "Hello.."
 echo "Before error.."
-ccibjchlikjnjk;dnf
+ccibjchlikjnjk;dnf # here shell understands there is an error and signal is ERR
 echo "After error"

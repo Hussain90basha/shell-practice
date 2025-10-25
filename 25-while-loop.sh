fa313bf4e0f95e511c8975d84f72f3275cff5a28
Hussain90basha <bashahussain9010@gmail.com>
2025-10-25 16:58:40 +0530

    practice

--- 25-while-loop.sh
+++ 25-while-loop.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-    echo "Starting countdown..."
-while [ $count -gt 0 ]
-do
-    echo "Time left: $count"
-    sleep 1 #pause for 1 sec
-    count=$((count -1)) #Decrement the count
-done
+#     echo "Starting countdown..."
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left: $count"
+#     sleep 1 #pause for 1 sec
+#     count=$((count -1)) #Decrement the count
+# done
 
-echo "Times up!"
+# echo "Times up!"
+
+while IFS= read -r line; do
+    # Process each line here
+    echo "Processing line: $line"
+    # Example: you can perform other operations with the $line variable
+    # For instance:
+    # some_command "$line"
+    # if [[ "$line" == "specific_text" ]]; then
+    #    echo "Found specific text!"
+    # fi
+done < 22-script-1.sh
